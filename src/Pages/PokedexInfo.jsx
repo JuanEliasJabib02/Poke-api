bed5e2287ecabc2e41f5a093ad4f6fe65d9792c5
JuanEliasJabib02 <juaneliasjabib02@gmail.com>
2023-01-23 14:55:17 -0500

    doing css

--- src/Pages/PokedexInfo.jsx
+++ src/Pages/PokedexInfo.jsx
@@ -18,11 +18,9 @@ const PokedexInfo = () => {
       .catch(err => console.log(err))   
   }, [id])
 
-  console.log(pokemon)
-  
 
   return (
-    <div>
+    <div className='pokedex-info'>
         <header className='pokedex-info__header'> 
         <img className='pokedex-info__header-img' src="/public/Home/homeBanner.png" alt="" />
         <div className='pokedex-info__header-low-side'></div>
@@ -31,63 +29,78 @@ const PokedexInfo = () => {
         </div>
       </header>
 
+     
+
       <article className='pokedex-info-card'>
-        <header className='pokedex-info-card__header'>
+        <header className={`pokedex-info-card__header bg-${pokemon?.types[0].type.name}`}>
           <img className="pokedex-info-card__img" src={pokemon?.sprites.other["official-artwork"].front_default} alt="" />
-          <h2 className='pokedex-info-card__id'>
-            #{pokemon?.id}
-          </h2>
-          <h3 className='pokedex-info-card__name'>
+        </header>
+        <section className='pokedex-info-card__prebody'>
+            <h2 className={`pokedex-info-card__id color-${pokemon?.types[0].type.name}`}>
+              #{pokemon?.id}
+            </h2>
+          <h3 className={`pokedex-info-card__name color-${pokemon?.types[0].type.name}`}>
             {pokemon?.name}
           </h3>
           <ul className='pokedex-info-card__base-info'>
             <li className='pokedex-info-card__weight'>
               <span>Weight</span>
-              {pokemon?.weight}
+              <p>{pokemon?.weight}</p>
             </li>
             <li className="pokedex-info-card__height">
               <span>Height</span>
-              {pokemon?.height}
+              <p>{pokemon?.height}</p>
             </li>
           </ul>
-        </header>
+        </section>
         <section className='pokedex-info-card__body'>
+
           <ul className='pokedex-info-card__types-container'>
             <p className='pokedex-info-card__type-label'>Type</p>
+
+            <div className='pokedex-info-card__type-container'>
           {
               pokemon?.types.map(type => (
-                <li className='pokedex-info-card__type' key={type.type.name}>
+                <li className={`pokedex-info-card__type type-${type.type.name}`}key={type.type.name}>
                   <p>{type.type.name}</p>
                 </li>
               ))
-          }
+              }
+              </div>
           </ul>
 
           <ul className='pokedex-info-card__ability-container'>
             <p className='pokedex-info-card__ability-label'>abilities</p>
-            {
-              pokemon?.abilities.map(ability => (
-                <li key={ability.ability.name } className='pokedex-info-card__ability'>
-                  <p>{ability.ability.name}</p>
-                </li>
-              ))
-            }
+
+            <div className='pokedex-info-card__ability-container-item'>
+              {
+                pokemon?.abilities.map(ability => (
+                  <li key={ability.ability.name } className='pokedex-info-card__ability'>
+                    <p className='ability__item'>{ability.ability.name}</p>
+                  </li>
+                ))
+                }
+            </div>
           </ul>
-              
-          <ul className='poke-card__stats-container'>
+          </section>
+          
+          <footer className='poke-info-card__footer'>
+          <ul className='poke-info-card__stats-container'>
             {
               pokemon?.stats.map(stat => (
-                <li className='poke-card__stat' key={stat.stat.name}>
-                  <span className='poke-card__name-stat'>{stat.stat.name}</span>
-                  <span className={`poke-card__value-stat color-${pokemon?.types[0].type.name}`}>{stat.base_stat}</span>
+                <li className='poke-info-card__stat' key={stat.stat.name}>
+                  <span className='poke-info-card__name-stat'>{stat.stat.name}</span>
+                  <span className={`poke-info-card__value-stat color-${pokemon?.types[0].type.name}`}>{stat.base_stat}</span>
                 </li>
               ))
             }
-          </ul>
-        </section>
+            </ul>
+            </footer>
+        
+      </article>
 
-        <footer className='pokedex-info-card__movements'>
-          <ul className='poke-info__moves-container'>
+      <article className='pokedex-moves-card'>
+          <ul className='pokdex-moves-card__container'>
             {
               pokemon?.moves.map(move => (
                 <li>
@@ -97,8 +110,7 @@ const PokedexInfo = () => {
             }
           </ul>
 
-        </footer>
-      </article>
+        </article>
       
     </div>
   )
